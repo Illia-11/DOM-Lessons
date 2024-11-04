2c6ed3a6c257ba527386a0b7d5857a8d8c165bb9
Illia Piddubnyi <illya2008zp@gmail.com>
2024-11-04 18:32:19 +0200

    Add bonus level solution

--- assets/JS/tasks.js
+++ assets/JS/tasks.js
@@ -1,15 +1,26 @@
 // hometask 01.11.2024
+/*
+basic level:
+  зробити в html файлі кнопку
+  при натисканні на кнопку ви маєта алертом виводити кількість кліків на цю кнопку
 
-const buttons = document.getElementsByTagName('button')
-const [clickMeBtn] = buttons
+bonus level:
+  приховати від зовншього коду кількість кліків
+*/
 
-console.log(buttons)
+// basic && bonus level:
+const [clickMeBtn] = document.getElementsByTagName('button')
 
-let counter = 0
+function createAlertClickCounter() {
+  let counter = 0
 
-function alertClicksCounter() {
-  counter++
-  alert(counter)
+  function alertClicksCounter() {
+    alert(++counter)
+  }
+
+  return alertClicksCounter
 }
 
-clickMeBtn.addEventListener('click', alertClicksCounter)
+clickMeBtn.addEventListener('click', createAlertClickCounter())
+
+console.log('===============')
